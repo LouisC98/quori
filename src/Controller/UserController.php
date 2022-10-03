5938cb4173ff52ced1f219d77aeebc046d7111b1
Louis Carvalho <louis.carvalho98@gmail.com>
2022-10-03 19:21:27 +0200

    mise en place de la page profil + possibilité de maj de la photo de profil

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -3,10 +3,13 @@
 namespace App\Controller;
 
 use App\Entity\User;
+use App\Form\RegistrationFormType;
 use App\Repository\CommentRepository;
 use App\Repository\QuestionRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -14,6 +17,42 @@ use Symfony\Component\Routing\Annotation\Route;
 #[IsGranted("ROLE_USER")]
 class UserController extends AbstractController
 {
+    #[Route('/', name: 'app_user')]
+    public function index(Request $request, EntityManagerInterface $em) {
+
+        $user = $this->getUser();
+
+        $userForm = $this->createForm(RegistrationFormType::class, $user);
+        $userForm->remove('email')
+                ->remove('password');
+
+        $userForm->handleRequest($request);
+
+        if ($userForm->isSubmitted() && $userForm->isValid()) {
+
+            $newPicture = $userForm->get('picture')->getData();
+            
+            if ($newPicture) {
+                
+                $oldPicture = $user->getPicture();
+                $extension = $newPicture->guessExtension();
+                $filename = bin2hex(random_bytes(6)) . '.' . $extension;
+                $folder = $this->getParameter('pictures.folder');
+
+                $user->setPicture($filename);
+                $newPicture->move($folder, $filename);
+
+                if($oldPicture !== 'default.jpg') {
+                    unlink($folder .'/'. $oldPicture);
+                } 
+            } 
+
+            $em->flush();
+        }
+
+        return $this->render('user/index.html.twig', ['form' => $userForm->createView()]);
+    }
+
     #[Route('/questions/{id}', name: 'app_user_questions')]
     public function questions(User $user, QuestionRepository $questionRepo): Response
     {
